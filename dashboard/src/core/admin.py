7c4650f5665c83c41384f581c9cd8bd5f638ba62
Deepak Panday <rdeep257@gmail.com>
2025-07-09 08:39:01 +0530

    admin

--- dashboard/src/core/admin.py
+++ dashboard/src/core/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import FileProcess, ApiCall
 
-# Register your models here.
+@admin.register(FileProcess)
+class FileProcessAdmin(admin.ModelAdmin):
+    list_display = ('filename', 'status', 'created_at', 'updated_at', 'unique_id')
+    list_filter = ('status', 'created_at')
+    search_fields = ('filename', 'unique_id')
+    readonly_fields = ('unique_id', 'created_at', 'updated_at')
+    ordering = ('-created_at',)
+
+@admin.register(ApiCall)
+class ApiCallAdmin(admin.ModelAdmin):
+    list_display = ('file_process', 'row_number', 'api_status', 'created_at', 'is_successful')
+    list_filter = ('api_status', 'created_at', 'file_process__status')
+    search_fields = ('file_process__filename', 'error_message')
+    readonly_fields = ('created_at',)
+    ordering = ('-created_at',)
+    
+    def is_successful(self, obj):
+        return obj.is_successful
+    is_successful.boolean = True
+    is_successful.short_description = 'Successful'
